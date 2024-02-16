a3bfdab296e47e5ba966a50822d6827e932274db
MonicaAnanthan <monicaananthan27@gmail.com>
2024-02-16 21:19:17 +0530

    authenticateUser

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,13 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authenticateUser = (req, res, next) => {
+
+  if (req.path === '/' || req.path.startsWith('/auth')) {
+    return next();
+  }
+  
   const token = req.headers.authorization?.split(' ')[1];
+  console.log('Request Headers:', req.headers);
   if (!token) {
     return res.status(401).json({ message: 'Authentication required' });
   }
